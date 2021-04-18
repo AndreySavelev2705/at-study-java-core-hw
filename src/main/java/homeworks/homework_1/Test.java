f4aa421d166df953185958c7a666084be10e5f1b
AndreySavelev2705 <savelev.andrey.job.mail@gmail.com>
2021-04-18 13:19:03 +0300

    Домашняя работа по первой леции.

--- src/main/java/homeworks/homework_1/Test.java
+++ src/main/java/homeworks/homework_1/Test.java
@@ -7,9 +7,9 @@ public class Test {
 
         Circle circle = new Circle();
 
-        System.out.println(circle.getAreaOfACircle(0));
-        System.out.println(circle.getAreaOfACircle(2));
-        System.out.println(circle.getAreaOfACircle(100));
+        System.out.println("Входные данные: 0; Площадь окружности равна: " + circle.getAreaOfACircle(0));
+        System.out.println("Входные данные: 2; Площадь окружности равна: " + circle.getAreaOfACircle(2));
+        System.out.println("Входные данные: 100; Площадь окружности равна: " + circle.getAreaOfACircle(100));
 
         System.out.println();
 
@@ -17,9 +17,14 @@ public class Test {
 
         Rectangle rectangle = new Rectangle();
 
-        System.out.println(rectangle.isAreaBiggerThenPerimeter(2,7));
-        System.out.println(rectangle.isAreaBiggerThenPerimeter(6,5));
-        System.out.println(rectangle.isAreaBiggerThenPerimeter(6,3));
+        System.out.println("Входные данные: 2 и 7; Площадь прямоугольника меньше его периметра так как возвращается: "
+                + rectangle.isAreaBiggerThenPerimeter(2,7));
+
+        System.out.println("Входные данные: 2 и 7; Площадь прямоугольника больше его периметра так как возвращается: "
+                + rectangle.isAreaBiggerThenPerimeter(6,5));
+
+        System.out.println("Входные данные: 6 и 3; Площадь прямоугольника меньше его периметра так как возвращается: "
+                + rectangle.isAreaBiggerThenPerimeter(6,3));
 
         System.out.println();
 
@@ -27,8 +32,9 @@ public class Test {
 
         Number number = new Number();
 
-        System.out.println(number.isTheNumberOddOrEven(14));
-        System.out.println(number.isTheNumberOddOrEven(19));
+        System.out.println("Входные данные: 14; Число четное так как возвращается: " + number.isTheNumberOddOrEven(14));
+        System.out.println("Входные данные: 19; Число нечетное так как возвращается: "
+                + number.isTheNumberOddOrEven(19));
 
         System.out.println();
 
@@ -36,9 +42,9 @@ public class Test {
 
         Number number1 = new Number();
 
-        System.out.println(number1.lengthInMeters(115));
-        System.out.println(number1.lengthInMeters(800));
-        System.out.println(number1.lengthInMeters(278));
+        System.out.println("Входные данные: 115см; В целых метрах это: " + number1.lengthInMeters(115));
+        System.out.println("Входные данные: 800см; В целых метрах это: " + number1.lengthInMeters(800));
+        System.out.println("Входные данные: 278см; В целых метрах это: " + number1.lengthInMeters(278));
 
         System.out.println();
 
@@ -50,20 +56,34 @@ public class Test {
 
         LogicalCalculation logicalCalculation = new LogicalCalculation();
 
-        System.out.println(logicalCalculation.getTheInversionOfALogicalOr(false, false));
-        System.out.println(logicalCalculation.getTheInversionOfALogicalOr(false, true));
-        System.out.println(logicalCalculation.getTheInversionOfALogicalOr(true, false));
-        System.out.println(logicalCalculation.getTheInversionOfALogicalOr(true, true));
+        System.out.println("Входные данные: false, false; результат логического ИЛИ: "
+                + logicalCalculation.getTheInversionOfALogicalOr(false, false));
+
+        System.out.println("Входные данные: false, true; результат логического ИЛИ: "
+                + logicalCalculation.getTheInversionOfALogicalOr(false, true));
+
+        System.out.println("Входные данные: true, false; результат логического ИЛИ: "
+                + logicalCalculation.getTheInversionOfALogicalOr(true, false));
+
+        System.out.println("Входные данные: true, true; результат логического ИЛИ: "
+                + logicalCalculation.getTheInversionOfALogicalOr(true, true));
 
         System.out.println();
         System.out.println("Второй метод");
 
         LogicalCalculation logicalCalculation1 = new LogicalCalculation();
 
-        System.out.println(logicalCalculation1.getLogicalAndWithInvertedParameters(false, false));
-        System.out.println(logicalCalculation1.getLogicalAndWithInvertedParameters(false, true));
-        System.out.println(logicalCalculation1.getLogicalAndWithInvertedParameters(true, false));
-        System.out.println(logicalCalculation1.getLogicalAndWithInvertedParameters(true, true));
+        System.out.println("Входные данные: false, false; результат логического И между инвертированными элементами: "
+                + logicalCalculation1.getLogicalAndWithInvertedParameters(false, false));
+
+        System.out.println("Входные данные: false, true; результат логического И между инвертированными элементами: "
+                + logicalCalculation1.getLogicalAndWithInvertedParameters(false, true));
+
+        System.out.println("Входные данные: true, false; результат логического И между инвертированными элементами: "
+                + logicalCalculation1.getLogicalAndWithInvertedParameters(true, false));
+
+        System.out.println("Входные данные: true, true; результат логического И между инвертированными элементами: "
+                + logicalCalculation1.getLogicalAndWithInvertedParameters(true, true));
 
         System.out.println();
 
@@ -73,10 +93,19 @@ public class Test {
 
         LogicalCalculation logicalCalculation2 = new LogicalCalculation();
 
-        System.out.println(logicalCalculation2.schematicDiagram(false, false, false, false));
-        System.out.println(logicalCalculation2.schematicDiagram(true, true, true, true));
-        System.out.println(logicalCalculation2.schematicDiagram(false, true, false, true));
-        System.out.println(logicalCalculation2.schematicDiagram(true, false, true, false));
-        System.out.println(logicalCalculation2.schematicDiagram(true, true, false, false));
+        System.out.println("Входные данные на принципиальную схему: false, false, false, false; Результат: "
+                + logicalCalculation2.schematicDiagram(false, false, false, false));
+
+        System.out.println("Входные данные на принципиальную схему: true, true, true, true; Результат: "
+                + logicalCalculation2.schematicDiagram(true, true, true, true));
+
+        System.out.println("Входные данные на принципиальную схему: false, true, false, true; Результат: "
+                +logicalCalculation2.schematicDiagram(false, true, false, true));
+
+        System.out.println("Входные данные на принципиальную схему: true, false, true, false; Результат: "
+                +logicalCalculation2.schematicDiagram(true, false, true, false));
+
+        System.out.println("Входные данные на принципиальную схему: true, true, false, false; Результат: "
+                +logicalCalculation2.schematicDiagram(true, true, false, false));
     }
 }
