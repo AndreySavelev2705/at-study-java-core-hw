b8dec4bebf014fd2f9e9809471cb9f2baf5193a5
AndreySavelev2705 <46755844+AndreySavelev2705@users.noreply.github.com>
2021-05-30 19:50:06 +0300

    Delete src/main/java/homeworks/homework_4/task_1 directory

--- src/main/java/homeworks/homework_4/task_1/ApplicationHuman.java
+++
@@ -1,9 +0,0 @@
-package homeworks.homework_4.task_1;
-
-public class ApplicationHuman {
-    public static void main(String[] args) {
-        Human human = new Human();
-
-        human.setFirstName("Андрей");
-    }
-}
