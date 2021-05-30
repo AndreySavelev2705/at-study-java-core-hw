416e9a32f65004202717ff71eb6c92820db9344b
AndreySavelev2705 <savelev.andrey.job.mail@gmail.com>
2021-05-30 23:23:08 +0300

    Домашние задание

---
+++ src/main/java/homeworks/homework_4/task_1/ApplicationHuman.java
@@ -0,0 +1,9 @@
+package homeworks.homework_4.task_1;
+
+public class ApplicationHuman {
+    public static void main(String[] args) {
+        Human human = new Human();
+
+        human.setFirstName("Андрей");
+    }
+}
